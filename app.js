34d85a651df95c4b3a74f787d24e831bf73c89d1
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-07 11:43:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+alert ("Boas vindas ao nosso site");
+let nome = "Lua";
+let idade = 25;
+let saldoDeVendas = 50;
+let saldoDisponivel = 1000;
+alert ("Erro! Preencha todos os campos");
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert
