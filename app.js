a7aa971d4bed26b1a0e7e4b8ede4c33737af0cb4
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-09 08:43:57 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 alert ("Boas vindas ao nosso site");
-let nome = "Lua";
-let idade = 25;
-let saldoDeVendas = 50;
-let saldoDisponivel = 1000;
-alert ("Erro! Preencha todos os campos");
-let mensagemDeErro = "Erro! Preencha todos os campos";
-alert
+// let nome = "Lua";
+// let idade = 25;
+// let saldoDeVendas = 50;
+// let saldoDisponivel = 1000;
+// alert ("Erro! Preencha todos os campos");
+// let mensagemDeErro = "Erro! Preencha todos os campos";
+let nome = prompt ("Qual o seu nome");
+let idade = prompt ("Qual sua idade");
+if (idade >= 18) {
+    alert ("Pode tirar a habilitação!");
+} else {
+    alert ("Não pode tirar a habilitação!");
+}
