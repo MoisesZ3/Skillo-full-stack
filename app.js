748d69c2faae6d001da8b2a13a5dedd4b651168d
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-11 08:22:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-alert ("Boas vindas ao nosso site");
+// Desafio 1
+// alert ("Boas vindas ao nosso site");
 // let nome = "Lua";
 // let idade = 25;
 // let saldoDeVendas = 50;
 // let saldoDisponivel = 1000;
 // alert ("Erro! Preencha todos os campos");
 // let mensagemDeErro = "Erro! Preencha todos os campos";
-let nome = prompt ("Qual o seu nome");
-let idade = prompt ("Qual sua idade");
-if (idade >= 18) {
-    alert ("Pode tirar a habilitação!");
-} else {
-    alert ("Não pode tirar a habilitação!");
-}
+
+// let nome = prompt ("Qual o seu nome");
+// console.log (nome)
+// alert (`Ola ${nome}!`);
+// let idade = prompt ("Qual sua idade");
+// console.log (idade)
+// if (idade >= 18) {
+//      alert ("Pode tirar a habilitação!");
+// } else {
+//      alert ("Não pode tirar a habilitação!");
+// }
+
+// Desafio 2
+// let diaDaSemana = prompt ("Você pode me dizer que dia da semana é hoje");
+// if (diaDaSemana == "sabado"|| diaDaSemana == "domingo") {
+//     alert ("Bom fim de semana");
+// } else {
+//     alert ("Boa semana");
+// }
+
+// let numero = prompt ("Escolha qualquer número");
+// if (numero > -1) {
+//     alert ("O número " + numero + " é positivo");
+// } else {
+//     alert ("O número " + numero + " é negativo");
+// }
+
+// let pontuação = prompt ("Qual foi sua pontuação");
+// if (pontuação >= 100) {
+//     alert ("Parabéns, você venceu!");
+// } else {
+//     alert ("Tente novamente para ganhar.");
+// }
+
+// let saldoUsuario = 51000
+// let desejo = prompt ("O que você quer consultar");
+// if (desejo = "saldo") {
+//     alert ("seu saldo é " + saldoUsuario);
+// }
+
+// let nome = prompt ("Qual o seu nome");
+// alert ("Ola seja bem vindo" + nome );
