46eef01b16e76a9acf995f016836661c0b6971fd
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-11 08:26:53 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // Desafio 1
+// 1
 // alert ("Boas vindas ao nosso site");
+// 2
 // let nome = "Lua";
+// 3
 // let idade = 25;
+// 4
 // let saldoDeVendas = 50;
+// 5
 // let saldoDisponivel = 1000;
+// 6
 // alert ("Erro! Preencha todos os campos");
+// 7
 // let mensagemDeErro = "Erro! Preencha todos os campos";
-
+// 8
 // let nome = prompt ("Qual o seu nome");
 // console.log (nome)
 // alert (`Ola ${nome}!`);
+// 9-10
 // let idade = prompt ("Qual sua idade");
 // console.log (idade)
 // if (idade >= 18) {
@@ -19,32 +27,33 @@
 // }
 
 // Desafio 2
+//1
 // let diaDaSemana = prompt ("Você pode me dizer que dia da semana é hoje");
 // if (diaDaSemana == "sabado"|| diaDaSemana == "domingo") {
 //     alert ("Bom fim de semana");
 // } else {
 //     alert ("Boa semana");
 // }
-
+//2
 // let numero = prompt ("Escolha qualquer número");
 // if (numero > -1) {
 //     alert ("O número " + numero + " é positivo");
 // } else {
 //     alert ("O número " + numero + " é negativo");
 // }
-
+//3
 // let pontuação = prompt ("Qual foi sua pontuação");
 // if (pontuação >= 100) {
 //     alert ("Parabéns, você venceu!");
 // } else {
 //     alert ("Tente novamente para ganhar.");
 // }
-
+//4
 // let saldoUsuario = 51000
 // let desejo = prompt ("O que você quer consultar");
 // if (desejo = "saldo") {
 //     alert ("seu saldo é " + saldoUsuario);
 // }
-
+//5
 // let nome = prompt ("Qual o seu nome");
 // alert ("Ola seja bem vindo" + nome );
