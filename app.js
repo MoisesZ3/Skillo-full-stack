18da8efec780e970a81febf867c899c01ef10759
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-13 17:38:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 5;
+// console.log(numeroSecreto);
+// let chute;
+// let tentativas = 1;
+// // enquanto chute não for igual ao n.s.
+// while (chute != numeroSecreto) {
+//    chute = prompt('Escolha um número entre 1 e 10');
+//    // se chute for igual ao número secreto
+//    if (chute == numeroSecreto) { 
+//             alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
+//    } else {
+//        if (chute > numeroSecreto) {
+//            alert(`O número secreto é menor que ${chute}`);
+//         } else {
+//            alert(`0 número secreto é maior que ${chute}`);
+//         }  
+//         // tentativa = tentativas + 1 
+//         tentativas++  
+//     }
+// }
+
 // Desafio 1
 // 1
 // alert ("Boas vindas ao nosso site");
@@ -57,3 +79,44 @@
 //5
 // let nome = prompt ("Qual o seu nome");
 // alert ("Ola seja bem vindo" + nome );
+
+// Desafio 3
+// 1
+// alert("Clique no botão OK 10 vezes e receba uma surpresa!");
+// let contador = 1
+// console.log (contador)
+// while (contador <= 10) {
+//       alert("Você ja clicou " + contador + " vezes.")
+//     if (contador == 10) {
+//        alert ("Parabens você perdeu mais ou menos 10 segundos da sua vida.")
+//     }
+//     contador++
+// }
+
+//2
+// alert("Receita federal esta a 10km de você");
+// let contador = 10
+// while (contador >= 0) {
+//        alert(contador + "km")
+//     if (contador == 0) {
+//       alert ("Você não pagou os impostos e tomaram sua casa.")
+//     }
+//     contador--
+// }
+
+//3
+// let contador = prompt ("escolha um número")
+// console.log (contador)
+// while (contador > 0) {
+//     contador--
+//     console.log (contador)
+// }
+
+// 4
+// let numero = prompt ("escolha um número")
+// let contador = 0
+// console.log (contador)
+// while (contador < numero) {
+//     contador++
+//     console.log (contador)
+// }
