c6ecdd05392a4c054c7f0f99defe29f9bd46548d
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-15 08:52:30 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-// alert('Boas vindas ao jogo do número secreto');
-// let numeroSecreto = parseInt (Math.random() * 100 + 1);
-// console.log(numeroSecreto);
-// let chute;
-// let tentativas = 1;
-// // enquanto chute não for igual ao n.s.
-// while (chute != numeroSecreto) {
-//    chute = prompt('Escolha um número entre 1 e 100');
-//    // se chute for igual ao número secreto
-//    if (chute == numeroSecreto) { 
-//             alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
-//    } else {
-//        if (chute > numeroSecreto) {
-//            alert(`O número secreto é menor que ${chute}`);
-//         } else {
-//            alert(`0 número secreto é maior que ${chute}`);
-//         }  
-//         // tentativa = tentativas + 1 
-//         tentativas++;  
-//     }
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt (Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+   chute = prompt('Escolha um número entre 1 e 100');
+   // se chute for igual ao número secreto
+   if (chute == numeroSecreto) { 
+        break;
+   } else {
+       if (chute > numeroSecreto) {
+           alert(`O número secreto é menor que ${chute}`);
+        } else {
+           alert(`0 número secreto é maior que ${chute}`);
+        }  
+        // tentativa = tentativas + 1 
+        tentativas++;  
+    }
+}
+
+let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativas}`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativa`);
 // }
 
 // DESAFIO 1
