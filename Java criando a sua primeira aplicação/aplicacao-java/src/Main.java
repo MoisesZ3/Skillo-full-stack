e366bfaee5f5ce46e102599e1bd88ac5f1683bf8
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-30 11:41:56 -0300

    Add files via upload

---
+++ Java criando a sua primeira aplicação/aplicacao-java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String saudacao = "e ";
+        String nome = "e ";
+        String continuacao = "e ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
+    }
+}
