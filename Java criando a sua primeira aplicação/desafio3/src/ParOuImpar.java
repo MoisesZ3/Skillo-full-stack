5b33cf0162745e35e4ecdadef13fb88ec25f889e
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-07-31 19:19:36 -0300

    Add files via upload

--- Java criando a sua primeira aplicação/desafio3/src/ParOuImpar.java
+++ Java criando a sua primeira aplicação/desafio3/src/ParOuImpar.java
@@ -4,7 +4,12 @@ public class ParOuImpar {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
         int numero = leitura.nextInt();
-        int resultado = numero / 2;
-        System.out.println(resultado);
+        int calculo = numero % 2;
+
+        if (calculo == 0) {
+            System.out.println("O número " + numero + " é par");
+        } else {
+            System.out.println("O número " + numero + " é impar");
+        }
     }
 }
