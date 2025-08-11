35dd82acbfc8b174dc2336a2877340842ee86563
MoisesZ3 <moisesdutrateixeira@gmail.com>
2025-08-11 11:53:51 -0300

    Add files via upload

--- Java aplicando a Orientação a Objetos Java aplicando a Orientação a Objetos/desafio3/src/MainNumerosPrimos.java
+++ Java aplicando a Orientação a Objetos Java aplicando a Orientação a Objetos/desafio3/src/MainNumerosPrimos.java
@@ -1,15 +1,18 @@
 import br.com.alura.desafio3.primos.GeradorPrimo;
 import br.com.alura.desafio3.primos.NumerosPrimos;
 
+import java.util.Scanner;
+
 public class MainNumerosPrimos {
     public static void main(String[] args) {
-//        NumerosPrimos testarNumero = new NumerosPrimos();
-//        testarNumero.setNumero(2);
+        Scanner leitura = new Scanner(System.in);
+        NumerosPrimos testarNumero = new NumerosPrimos();
+        testarNumero.setNumero(leitura.nextInt());
+
+        testarNumero.verificarPrimalidade();
+
+//        GeradorPrimo testeNumero = new GeradorPrimo();
 //
-//        testarNumero.verificarPrimalidade();
-
-        GeradorPrimo testeNumero = new GeradorPrimo();
-
-        testeNumero.gerarProximoPrimo();
+//        testeNumero.gerarProximoPrimo();
     }
 }
