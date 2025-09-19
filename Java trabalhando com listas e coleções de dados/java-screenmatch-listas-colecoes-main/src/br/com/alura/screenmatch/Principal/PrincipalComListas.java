934ea82f85f8654eca3ca24fe1df6621fa24743b
Moisés Dutra Teixeira <moisesdutrateixeira@gmail.com>
2025-09-19 11:56:27 -0300

    Add files via upload

--- Java trabalhando com listas e coleções de dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
+++ Java trabalhando com listas e coleções de dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -5,6 +5,8 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -24,5 +26,19 @@ public class PrincipalComListas {
                 System.out.println("Classificação " + filme.getClassificacao());
             }
         }
+        ArrayList<String> buscarPorArtista = new ArrayList<>();
+        buscarPorArtista.add("Adam Sandler");
+        buscarPorArtista.add("Paulo");
+        buscarPorArtista.add("Jacqueline");
+        System.out.println(buscarPorArtista);
+
+        Collections.sort(buscarPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscarPorArtista);
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenandi por ano");
+        System.out.println(lista);
     }
 }
